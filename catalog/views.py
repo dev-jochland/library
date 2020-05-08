9c39b19aeef745082c1c47129a18648695e7e034
Joel Onojason <joel.onojason@outlook.com>
2020-05-08 01:10:59 +0100

    Created views and template

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from .models import Author, Book, BookInstance, Genre, Language
+from django.views import generic
+
 
 # Create your views here.
+def index(request):
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available book (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default
+    num_authors = Author.objects.count()
+    num_genres = Genre.objects.values_list('name', flat=True).distinct().count()
+    num_language = Language.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres': num_genres,
+        'num_language': num_language,
+    }
+
+    return render(request, 'index.html', context)
+
+
+# Class based views automatically look for templates names in:
+# /Project_folder/App_Folder/templates/created_same_app_name_folder/<template_name>.html
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'list_of_books'
+    template_name = 'catalog/book.html'
--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls')),
-    path('', RedirectView.as_view(url='catalog/')),
-]+ static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('catalog/', include('catalog.urls')),
+                  path('', RedirectView.as_view(url='catalog/')),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
